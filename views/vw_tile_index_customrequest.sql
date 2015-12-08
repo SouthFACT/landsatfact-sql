c20c732f77779f646cf4c54480113f5fdf65931a
Dave Michelson <daveism@gmail.com>
2015-12-08 09:49:27 -0500

    update custom request tile

--- views/vw_tile_index_customrequest.sql
+++ views/vw_tile_index_customrequest.sql
@@ -1,21 +1,23 @@
-﻿CREATE OR REPLACE VIEW public.vw_tile_index_customrequest AS 
-	SELECT 
-	  '/lsfdata/products/'::text || 
+﻿CREATE OR REPLACE VIEW public.vw_tile_index_customrequest AS
+	SELECT
+	  '/lsfdata/products/'::text ||
 		(CASE WHEN product_type = 'GAP'OR  product_type = 'CLOUD'
-			THEN  lower(product_type) || '_mask' ELSE  lower(product_type)    END) ::text 
+			THEN  lower(product_type) || '_mask' ELSE  lower(product_type)    END) ::text
 		||  '/'::text || products.product_id::text AS location,
 	    extracted_imagery.quad_id AS oid,
 	    lq.geom,
 	    products.product_date,
 	    wc.proj_wkt AS srs,
-	    extracted_imagery.process_date
+	    extracted_imagery.process_date,
+			(((date (SUBSTR(products.input1::text, 10, 4) || '-01-01')::date +  interval '1 day' * SUBSTR(products.input1::text, 14, 3)::integer  )::date -  interval '1 day')::date)::text || '_' ||
+		  (((date (SUBSTR(products.input2::text, 10, 4) || '-01-01')::date +  interval '1 day' * SUBSTR(products.input2::text, 14, 3)::integer  )::date -  interval '1 day')::date)::text scene_dates
 	   FROM products,
 	    landsat_quads lq,
 	    extracted_imagery,
 	    wrs2_codes wc
 	  WHERE analysis_source = 'CR'
-	  AND products.input1::text = extracted_imagery.quad_scene::text 
-	  AND extracted_imagery.quad_id::text = lq.quad_id::text 
+	  AND products.input1::text = extracted_imagery.quad_scene::text
+	  AND extracted_imagery.quad_id::text = lq.quad_id::text
 	  AND lq.wrs2_code::text = wc.wrs2_code::text
 	ORDER BY products.product_date DESC;
 
