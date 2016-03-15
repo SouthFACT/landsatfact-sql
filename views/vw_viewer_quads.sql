45aa1ff6f39a15ab12be637ec264216f11d4d839
mwhutchi <mwhutchi@unca.edu>
2016-03-15 11:02:56 -0400

    Update vw_viewer_quads.sql

--- views/vw_viewer_quads.sql
+++ views/vw_viewer_quads.sql
@@ -2,22 +2,17 @@
 
 -- DROP VIEW public.vw_viewer_quads;
 
-CREATE OR REPLACE VIEW public.vw_viewer_quads AS
- SELECT extracted_imagery.quad_id AS oid,
-    lq.geom,
-    max(products.product_date) AS last_update,
-    wc.proj_wkt AS srs,
-    products.input1,
-    (date (SUBSTR(products.input1::text, 10, 4) || '-01-01')::date  + ((SUBSTR(products.input1::text, 14, 3)::integer)-1)::integer  )::date input1_date,
-    products.input2,
-    (date (SUBSTR(products.input2::text, 10, 4) || '-01-01')::date  + ((SUBSTR(products.input2::text, 14, 3)::integer)-1)::integer  )::date input2_date
-   FROM extracted_imagery,
-    landsat_quads lq,
-    wrs2_codes wc,
-    products
-  WHERE products.input1::text = extracted_imagery.quad_scene::text AND extracted_imagery.quad_id::text = lq.quad_id::text AND lq.wrs2_code::text = wc.wrs2_code::text
-  GROUP BY extracted_imagery.quad_id, lq.geom, wc.proj_wkt, products.input1, products.input2
-  ORDER BY max(products.product_date) DESC;
+CREATE OR REPLACE VIEW public.vw_viewer_quads AS 
+ SELECT landsat_quads.gid AS oid,
+    landsat_quads.quad_id,
+    landsat_quads.geom,
+    vw_quad_latest_update.last_update,
+    vw_quad_latest_update.input1,
+    vw_quad_latest_update.input1_date,
+    vw_quad_latest_update.input2,
+    vw_quad_latest_update.input2_date
+   FROM landsat_quads
+   LEFT JOIN vw_quad_latest_update ON landsat_quads.quad_id::text = vw_quad_latest_update.oid::text;
 
 ALTER TABLE public.vw_viewer_quads
   OWNER TO root;
