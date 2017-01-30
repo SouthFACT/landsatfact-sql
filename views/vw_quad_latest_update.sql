b50c0e0c974b9666a40b176564fc4be1bbc1d642
Dave Michelson <daveism@gmail.com>
2017-01-30 09:25:11 -0500

    update performance

--- views/vw_quad_latest_update.sql
+++ views/vw_quad_latest_update.sql
@@ -9,29 +9,25 @@ CREATE OR REPLACE VIEW public.vw_quad_latest_update AS
 	  lq.geom,
 	  max(products.product_date) AS last_update,
 	  row_number() OVER (PARTITION BY extracted_imagery.quad_id ORDER BY max(products.product_date) DESC) AS rank,
-	  wc.proj_wkt AS srs,
+	  ''::character varying(600) as srs,
 	  products.input1,
 	  ((substr(products.input1::text, 10, 4) || '-01-01'::text)::date) + (substr(products.input1::text, 14, 3)::integer - 1) AS input1_date,
 	  products.input2,
 	  ((substr(products.input2::text, 10, 4) || '-01-01'::text)::date) + (substr(products.input2::text, 14, 3)::integer - 1) AS input2_date
-	FROM extracted_imagery,
-	  landsat_quads lq,
-	  wrs2_codes wc,
-	  products
-	WHERE products.input1::text = extracted_imagery.quad_scene::text AND
-	  extracted_imagery.quad_id::text = lq.quad_id::text AND
-	  lq.wrs2_code::text = wc.wrs2_code::text AND products.analysis_source::text = 'LCV'::text
-	  AND (products.disk_status::text = 'on_disk'::text OR products.product_date > '2016-06-11'::date)
+	FROM
+	   products
+           JOIN extracted_imagery ON  products.input1 = extracted_imagery.quad_scene
+           JOIN landsat_quads as lq ON  extracted_imagery.quad_id = lq.quad_id
+	WHERE
+	  products.analysis_source = 'LCV'
+	  AND (products.disk_status = 'on_disk' OR products.product_date > '2016-06-11'::date)
 	GROUP BY
 	   products.input1,
 	   products.input2,
 	   extracted_imagery.quad_id,
 	   lq.geom,
-	   wc.proj_wkt,
 	  ((substr(products.input1::text, 10, 4) || '-01-01'::text)::date) + (substr(products.input1::text, 14, 3)::integer - 1),
 	  ((substr(products.input2::text, 10, 4) || '-01-01'::text)::date) + (substr(products.input2::text, 14, 3)::integer - 1)
-	ORDER BY extracted_imagery.quad_id,
-	  row_number() OVER (PARTITION BY extracted_imagery.quad_id ORDER BY max(products.product_date) DESC)
        )
  SELECT latestquads.oid,
     latestquads.geom,
