f9319f05fe098583d1dc78c980cdacf3e485a4e7
mwhutchi <mwhutchi@unca.edu>
2017-04-04 16:31:19 -0400

    Update vw_custom_requests_for_viewer.sql

--- views/vw_custom_requests_for_viewer.sql
+++ views/vw_custom_requests_for_viewer.sql
@@ -1,5 +1,5 @@
-CREATE OR REPLACE VIEW public.vw_custom_requests_for_viewer AS 
- SELECT extracted_imagery.quad_id AS oid,
+CREATE OR REPLACE VIEW public.vw_custom_requests_for_viewer AS
+SELECT extracted_imagery.quad_id AS oid,
     lq.geom,
     products.product_date,
     products.product_type,
@@ -7,7 +7,8 @@ CREATE OR REPLACE VIEW public.vw_custom_requests_for_viewer AS
     lq.quad_id,
     c.aoi_id AS aoi,
     rtrim(custom_requests.request_id::text, '.zip'::text) AS request_id,
-    custom_request_dates.custom_request_date
+    custom_request_dates.custom_request_date,
+    extracted_imagery.process_date
    FROM products,
     landsat_quads lq,
     extracted_imagery,
@@ -16,7 +17,6 @@ CREATE OR REPLACE VIEW public.vw_custom_requests_for_viewer AS
     custom_requests,
     custom_request_dates
   WHERE custom_request_dates.custom_request_date > ('now'::text::date - '45 days'::interval day) AND products.analysis_source::text = 'CR'::text AND products.input1::text = extracted_imagery.quad_scene::text AND extracted_imagery.quad_id::text = lq.quad_id::text AND lq.wrs2_code::text = wc.wrs2_code::text AND c.scene_id::text = extracted_imagery.scene_id::text AND custom_requests.aoi_id = c.aoi_id AND custom_request_dates.aoi_id = c.aoi_id AND custom_request_dates.custom_request_status_id = 4;
-
 ALTER TABLE public.vw_custom_requests_for_viewer
   OWNER TO root;
 GRANT ALL ON TABLE public.vw_custom_requests_for_viewer TO root;
