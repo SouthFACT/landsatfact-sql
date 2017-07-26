87de954e0ac51a141fde85db5c0e124b52466489
mwhutchi <mwhutchi@unca.edu>
2017-07-26 17:19:37 -0400

    Update vw_archive_product_dates.sql

--- views/vw_archive_product_dates.sql
+++ views/vw_archive_product_dates.sql
@@ -2,12 +2,13 @@
 
 -- DROP VIEW public.vw_archive_product_dates;
 
-CREATE OR REPLACE VIEW public.vw_archive_product_dates AS
+CREATE OR REPLACE VIEW public.vw_archive_product_dates AS 
  SELECT DISTINCT products.product_date,
     products.product_type
    FROM products
-  WHERE coalesce(products.analysis_source,'')::text<> 'CR'
-ORDER BY products.product_date DESC;
+  WHERE (products.disk_status::text = 'on_disk'::text OR products.product_date > '2016-06-11'::date) AND COALESCE(products.analysis_source, ''::character varying)::text <> 'CR'::text AND products.product_date <= '2016-12-13'::date
+  ORDER BY products.product_date DESC;
+
 
 ALTER TABLE public.vw_archive_product_dates
   OWNER TO root;
